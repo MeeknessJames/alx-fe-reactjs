0508943913161b2897c8b62b59cffdd3f62fb0b3
Meekness James <meeknessjames1@gmail.com>
2025-10-12 19:45:12 +0100

    feat: implement dynamic route /blog/:id and protected route component

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,36 +1,24 @@
-import React, { useState } from "react";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import About from "./pages/About";
-import BlogPost from "./pages/BlogPost";
-import Profile from "./components/Profile";
+import Blog from "./pages/Blog";
+import BlogDetails from "./pages/BlogDetails";
+import Profile from "./pages/Profile";
 import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-
   return (
     <BrowserRouter>
-      <nav style={{ marginBottom: "20px" }}>
-        <Link to="/">Home</Link> |{" "}
-        <Link to="/about">About</Link> |{" "}
-        <Link to="/profile">Profile</Link> |{" "}
-        <Link to="/post/1">Blog Post</Link> |{" "}
-        <button onClick={() => setIsAuthenticated(!isAuthenticated)}>
-          {isAuthenticated ? "Logout" : "Login"}
-        </button>
-      </nav>
-
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/post/:id" element={<BlogPost />} />
 
-        {/* Protected Route Example */}
+        <Route path="/blog" element={<Blog />} />
+
+        <Route path="/blog/:id" element={<BlogDetails />} />
+
         <Route
-          path="/profile/*"
+          path="/profile"
           element={
-            <ProtectedRoute isAuthenticated={isAuthenticated}>
+            <ProtectedRoute>
               <Profile />
             </ProtectedRoute>
           }
